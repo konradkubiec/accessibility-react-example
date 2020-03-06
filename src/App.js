430c4c36a4880446b31bd5c846a670cc5047597b
Konrad Kubiec <konrad.kubiec@gmail.com>
2020-03-06 04:48:01 +0000

    completed task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
+import { LiveAnnouncer } from 'react-aria-live';
 import logo from './logo.svg';
 import './App.css';
+import Record from './Record.js';
+import Grid from './Grid.js';
 
 function App() {
   return (
+    <LiveAnnouncer>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="skip" href="#main">Skip to content</a>
       </header>
+      <section role="main">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 >Accessible React Example</h1>
+        <section id="main" tabIndex="-1">
+
+          <Record />
+        </section>
+        <hr/>
+        <section tabIndex="-1">
+          <Grid />
+        </section>
+      </section>
     </div>
+    </LiveAnnouncer>
   );
 }
 
